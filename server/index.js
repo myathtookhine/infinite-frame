5567acc3f87ec70b41f4cc896f0620132adaefe6
myathtookhine <myathtookhine@gmail.com>
2026-01-06 22:21:49 +0700

    fixed admin url

--- server/index.js
+++ server/index.js
@@ -9,8 +9,20 @@ const configRoutes = require('./routes/config');
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://admin.infiniteframe.online',
+    'http://localhost:5173',
+    'http://localhost:3000'
+  ],
+  credentials: true
+}));
 app.use(express.json());
+
+// API Routes အပေါ်နားမှာ ထည့်ပေးပါ
+app.get('/', (req, res) => {
+  res.send('Infinite Frame API is running peacefully...');
+});
 
 // API Routes
 app.use('/api', authRoutes); 
